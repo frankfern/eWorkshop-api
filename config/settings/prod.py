a0ecfa8104197b0f131cb19ebea0f60a8e9a3277
Frank Fernandez <frankfernogando@gmail.com>
2021-01-25 00:13:50 -0500

    fix db prod config

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,6 +9,7 @@ ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[
 DEBUG = False
 
 # Databases
+DATABASES['default'] = env.db('DATABASE_URL')  # NOQA
 DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # NOQA
 
 # Static  files
